1fcb89656a80c7c58695ce80dcb3eb2eebbc5767
tlswnsgk <110441845+tlswnsgk@users.noreply.github.com>
2022-08-17 12:13:39 +0900

    first commit

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,24 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+		// 주석 : 프로그램 실행에 영향을 미치지 않음
+		// 한 줄 주석
+		
+		/*여러 줄 주석
+		  여러 줄 주석~!
+		 
+		*/
+		// 단축키 : ctrl + /
+		// 힌트 얻기 : ctrl + space 
+		// 한 줄 지우기 : ctrl + d
+		// 코드 위 아래로 옮기기 : alt + ⬆/⬇		
+		// 콘솔 출력 코드 : sysout / sout
+		
+		
+		
+		System.out.println("HelloWorld!");
+		System.out.println("자바는 쉽지 않다");
+		System.out.println("잘 배워서 써먹어 보자");
+	}
+}
