80667be15aedd270ae381a804fae1be5218b2fa6
tlswnsgk <110441845+tlswnsgk@users.noreply.github.com>
2022-09-08 17:43:45 +0900

    추석 전 찐막

---
+++ src/practice/장8/RemoteControlExample.java
@@ -0,0 +1,12 @@
+package practice.장8;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+			public void turnOn() { //실행문 }
+			public void turnOff() {//실행문}
+			public void setVolume(int volume) {//실행문}
+			};
+			}
+		}
+}
---
+++ src/practice/장8/Searchable.java
@@ -0,0 +1,5 @@
+package practice.장8;
+
+public interface Searchable {
+	void search(String url);
+}
